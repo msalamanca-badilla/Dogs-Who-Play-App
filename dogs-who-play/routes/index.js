f34a6d02b276411d05656ca4d15f1c4d9ea639cb
Marianne <m.salamanca21@live.ca>
2021-12-21 22:06:07 -0500

    add passport routers

--- dogs-who-play/routes/index.js
+++ dogs-who-play/routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+const passport = require('passport')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  res.redirect('/dogs');
 });
+
+router.get('/auth/google', passport.authenticate(
+  'google',
+  { scope: ['profile', 'email'] }
+));
+
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect : '/dogs',
+    failureRedirect : '/dogs'
+  }
+));
 
 module.exports = router;
