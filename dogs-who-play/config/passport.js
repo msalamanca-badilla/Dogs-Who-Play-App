7251e53f6cef6b345ac6f49fd0d14118b6b4c67c
Marianne <m.salamanca21@live.ca>
2021-12-22 19:13:33 -0500

    add passport fundtion

--- dogs-who-play/config/passport.js
+++ dogs-who-play/config/passport.js
@@ -8,7 +8,24 @@ passport.use(new GoogleStrategy({
     callbackURL: process.env.GOOGLE_CALLBACK
   },
   function(accessToken, refreshToken, profile, cb) {
-    Dog.findOne({googleId: profile.id})
+    Dog.findOne({ 'googleId': profile.id }, function(err, dog) {
+      if (err) return cb(err);
+      if (dog) {
+        return cb(null, dog);
+      } else {
+        // we have a new student via OAuth!
+        var newDog = new Dog({
+          name: profile.displayName,
+          email: profile.emails[0].value,
+          googleId: profile.id
+        });
+        newDog.save(function(err) {
+          if (err) return cb(err);
+          return cb(null, newDog);
+        });
+      }
+    });
   }
 ));
+
 
